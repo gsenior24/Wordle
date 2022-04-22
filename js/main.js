6634c8eafb9248a06594f4f8f5f80148dedae5ad
gsenior24 <georgesenior23@gmail.com>
2022-04-22 20:55:33 +0100

    Added squares, allow letter input

---
+++ js/main.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createSquares();
+
+    let guessedWords = [[]];     
+    let availableSpace = 1;
+
+    const keys = document.querySelectorAll(".keyboard-row button");
+
+    for (let i = 0; i < keys.length; i++)
+    {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key");
+
+            updateGuessedWords(letter);
+        };
+    }
+
+    function getCurrentWordArr()
+    {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1];
+    }
+
+    function updateGuessedWords(letter)
+    {
+        const currentWordArr = getCurrentWordArr()
+
+        if (currentWordArr && currentWordArr.length < 5)
+        {
+            currentWordArr.push(letter);
+            const availableSpaceEl = document.getElementById(String(availableSpace));
+            availableSpace = availableSpace + 1;
+            availableSpaceEl.textContent = letter;
+        }
+    }
+
+    function createSquares() {
+        const gameBoard = document.getElementById("board")
+
+        for (let i = 0; i < 30; i++)
+        {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("id", i + 1);
+            gameBoard.appendChild(square);
+        }
+    }
+
+
+})
